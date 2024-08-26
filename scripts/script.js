bc457c2ef66b4d5c5ce61ee90e9f1bfb792f00f5
yasmena <129227497+Yasmeen-Mahmoud@users.noreply.github.com>
2024-08-26 20:57:44 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+
+
+document.getElementById("bars").addEventListener("click", () => {
+    var navSection = document.getElementById("nav-section");
+    if(navSection.style.display === "block")
+    {
+        navSection.style.display = "none";
+    }
+    else
+    {
+        navSection.style.display = "block";
+    }
+});
+
+window.addEventListener("resize", () => {
+    var navSection = document.getElementById("nav-section");
+    if(window.innerWidth > 900)
+    {
+        navSection.style.display = "flex";
+    }
+    else
+    {
+        navSection.style.display = "none";
+    }
+});
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: 
+    {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    breakpoints:
+    {
+        0:
+        {
+            slidesPerView: 1,
+        },
+        800:
+        {
+            slidesPerView: 2,
+        },
+        1200:
+        {
+            slidesPerView: 3,
+        },
+    }
+});
